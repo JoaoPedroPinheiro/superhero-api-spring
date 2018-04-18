4857968a48aeb40dc5b658c0c768b67defee9634
Joao Pedro Pinheiro <joao@joaopinheiro.io>
2018-04-18 19:31:32 +0200

    Refactor in the Superhero Entity

--- src/main/java/io/joaopinheiro/superhero/Superhero.java
+++ src/main/java/io/joaopinheiro/superhero/Superhero.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,9 +21,65 @@ public class Superhero {
 	private String publisher;
 	private String pseudonym;
 	
-	@OneToMany
-	private List<Superhero> allies;
+	@ElementCollection(targetClass=String.class)
+	private List<String> allies;
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getPseudonym() {
+		return pseudonym;
+	}
+
+	public void setPseudonym(String pseudonym) {
+		this.pseudonym = pseudonym;
+	}
+
+	public List<Superhero> getAllies() {
+		return allies;
+	}
+
+	public void setAllies(List<Superhero> allies) {
+		this.allies = allies;
+	}
+
+	public List<String> getSuperpowers() {
+		return superpowers;
+	}
+
+	public void setSuperpowers(List<String> superpowers) {
+		this.superpowers = superpowers;
+	}
+
+	public Date getFirstAppearance() {
+		return firstAppearance;
+	}
+
+	public void setFirstAppearance(Date firstAppearance) {
+		this.firstAppearance = firstAppearance;
+	}
+
 	@ElementCollection(targetClass=String.class)
 	private List<String> superpowers;
 	
