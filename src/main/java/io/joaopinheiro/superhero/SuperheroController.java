d92e6b388d756af1ec23c6c07e7d1a64877866b4
Joao Pedro Pinheiro <joao@joaopinheiro.io>
2018-04-18 19:49:24 +0200

    Controller annotations and mehtod headers

--- src/main/java/io/joaopinheiro/superhero/SuperheroController.java
+++ src/main/java/io/joaopinheiro/superhero/SuperheroController.java
@@ -1,4 +1,9 @@
 package io.joaopinheiro.superhero;
+import java.util.Collection;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,6 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class SuperheroController {
 
+	@GetMapping(path="superheroes",  produces = "application/json")
+	public Collection<Superhero> getAllSuperheroes(){
+		return null;
+	}
 	
+	@GetMapping(path = "superheroes/{id}", produces = "application/json")
+	public Superhero getSuperheroDetails(String id) {
+		return null;
+	}
+	
+	@PostMapping(path="superheroes", consumes = "application/json")
+	public void createSuperhero(@RequestBody Superhero hero) {
+		
+	}
 	
 }
