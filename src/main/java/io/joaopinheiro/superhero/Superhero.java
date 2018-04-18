333248871abab00aa2d6103c4f73b70eab1045d8
Joao Pedro Pinheiro <joao@joaopinheiro.io>
2018-04-18 19:27:19 +0200

    Implemented Superhero Entity

--- src/main/java/io/joaopinheiro/superhero/Superhero.java
+++ src/main/java/io/joaopinheiro/superhero/Superhero.java
@@ -1,8 +1,15 @@
 package io.joaopinheiro.superhero;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name= "superhero")
@@ -11,5 +18,16 @@ public class Superhero {
 	@Id
 	private long id;
 	
+	private String name;
+	private String publisher;
+	private String pseudonym;
 	
+	@OneToMany
+	private List<Superhero> allies;
+	
+	@ElementCollection(targetClass=String.class)
+	private List<String> superpowers;
+	
+	@Temporal(TemporalType.DATE)
+	private Date firstAppearance;
 }
