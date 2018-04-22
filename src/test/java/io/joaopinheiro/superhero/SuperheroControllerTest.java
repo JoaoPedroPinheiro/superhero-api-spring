4a4c958e14d855acd2967f10c8903acbaba35e2e
Joao Pedro Pinheiro <joao@joaopinheiro.io>
2018-04-22 12:56:58 +0200

    More unit tests for the SuperheroController

--- src/test/java/io/joaopinheiro/superhero/SuperheroControllerTest.java
+++ src/test/java/io/joaopinheiro/superhero/SuperheroControllerTest.java
@@ -1,11 +1,11 @@
 package io.joaopinheiro.superhero;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +18,9 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -35,14 +33,28 @@ public class SuperheroControllerTest {
     @MockBean
     private SuperheroController controller;
 
+    ObjectMapper mapper = new ObjectMapper();
+
     @Test
-    public void getAllSuperheroes() throws Exception {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        List<Superhero> allSuperheroes = Arrays.asList(TestUtils.BATMAN);
-
+    public void getAllSuperheroesEmpty() throws Exception{
+        List<Superhero> allSuperheroes = Collections.emptyList();
         given(controller.getAllSuperheroes()).willReturn(allSuperheroes);
+
+
+        MvcResult result = mvc.perform(get("http://localhost:8080/superheroes").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)))
+                .andReturn();
+
+        List<Superhero> resultHeroes = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<Superhero>>(){});
+        assertEquals(resultHeroes, allSuperheroes);
+    }
+
+    @Test
+    public void getAllSuperheroesSingle() throws Exception {
+        List<Superhero> allSuperheroes = Arrays.asList(TestUtils.BATMAN);
+        given(controller.getAllSuperheroes()).willReturn(allSuperheroes);
+
 
         MvcResult result = mvc.perform(get("http://localhost:8080/superheroes").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -50,18 +62,35 @@ public class SuperheroControllerTest {
                 .andReturn();
 
         List<Superhero> resultHeroes = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<Superhero>>(){});
-
-
-
         assertEquals(resultHeroes, allSuperheroes);
-
-
-
-
     }
 
     @Test
-    public void getSuperheroDetails() {
+    public void getAllSuperheroesMultiple() throws Exception{
+        List<Superhero> allSuperheroes = Arrays.asList(TestUtils.BATMAN, TestUtils.HULK, TestUtils.SUPERMAN);
+        given(controller.getAllSuperheroes()).willReturn(allSuperheroes);
+
+
+        MvcResult result = mvc.perform(get("http://localhost:8080/superheroes").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andReturn();
+
+        List<Superhero> resultHeroes = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<Superhero>>(){});
+        assertEquals(resultHeroes, allSuperheroes);
+    }
+
+    @Test
+    public void getSuperheroDetailsSuccess() throws Exception{
+        Long id = TestUtils.BATMAN.getId();
+
+        given(controller.getSuperheroDetails(id)).willReturn(TestUtils.BATMAN);
+        MvcResult result = mvc.perform(get("http://localhost:8080/superheroes/" + id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Superhero resultHero = mapper.readValue(result.getResponse().getContentAsString(), Superhero.class);
+        assertEquals(TestUtils.BATMAN, resultHero);
     }
 
     @Test
--- src/test/java/io/joaopinheiro/superhero/TestUtils.java
+++ src/test/java/io/joaopinheiro/superhero/TestUtils.java
@@ -7,6 +7,7 @@ public class TestUtils {
 
     public static final Superhero BATMAN;
     public static final Superhero HULK;
+    public static final Superhero SUPERMAN;
 
     public static final String BATMAN_JSON = "{id=1, name=Bruce Wayne, publisher=DC Comics, pseudonym=Batman, allies=[\"Robin\",\"Catwoman\",\"Justice League\"], superpowers=[\"Money\",\"Discipline\"], firstAppearance=2018-04-21T12:01:58.293+0000}";
 
@@ -15,7 +16,7 @@ public class TestUtils {
         BATMAN.setId(1);
         BATMAN.setName("Bruce Wayne");
         BATMAN.setPseudonym("Batman");
-        BATMAN.setPublisher("DC Comics");
+        BATMAN.setPublisher("DC");
         BATMAN.setAllies(Arrays.asList("Robin", "Catwoman", "Justice League"));
         BATMAN.setSuperpowers(Arrays.asList("Money", "Discipline"));
         BATMAN.setFirstAppearance(new Date());
@@ -28,5 +29,14 @@ public class TestUtils {
         HULK.setAllies(Arrays.asList("Captain America", "Iron Man","The Avengers"));
         HULK.setSuperpowers(Arrays.asList("Mutant Strength", "Is always angry"));
         HULK.setFirstAppearance(new Date());
+
+        SUPERMAN = new Superhero();
+        SUPERMAN.setId(3);
+        SUPERMAN.setName("Clark Kent");
+        SUPERMAN.setPseudonym("Superman");
+        SUPERMAN.setPublisher("DC");
+        SUPERMAN.setAllies(Arrays.asList("Robin", "Alfred","Justice League"));
+        SUPERMAN.setSuperpowers(Arrays.asList("Flying", "Super Strength", "Laser Eyes"));
+        SUPERMAN.setFirstAppearance(new Date());
     }
 }
